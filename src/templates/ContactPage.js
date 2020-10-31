a7eb880d7533d3450dcb10eeaf18e361d9b39fe2
Christian Adam <christian.g.adam@gmail.com>
2020-10-31 15:37:00 -0300

    Search locations in google AR

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -32,7 +32,7 @@ export const ContactPageTemplate = ({
             {address && (
               <a
                 className="Contact--Details--Item"
-                href={`https://www.google.com.au/maps/search/${encodeURI(
+                href={`https://www.google.com.ar/maps/search/${encodeURI(
                   address
                 )}`}
                 target="_blank"
